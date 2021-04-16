a7ddb5f92560765ffa7d976cd822d8e52d2f5ea4
Dherkyan Ribeiro da Silva <dherkyan@gmail.com>
2021-04-16 02:24:55 -0400

    18. UserController Test

---
+++ src/test/java/com/softwarefoundation/walletapi/controller/UserControllerTest.java
@@ -0,0 +1,59 @@
+package com.softwarefoundation.walletapi.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.softwarefoundation.walletapi.entity.User;
+import com.softwarefoundation.walletapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class UserControllerTest {
+
+    static final String NOME = "Alice";
+    static final String EMAIL = "alice@gmail.com";
+    static final String SENHA = "123";
+    static final String URL = "/user";
+
+    @MockBean
+    UserService userService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    public void testSave() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .post(URL)
+                .content(getJsonPayload())
+                .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+    public User getUserMock(){
+        User user = new User();
+        user.setNome(NOME);
+        user.setEmail(EMAIL);
+        user.setSenha(SENHA);
+        return user;
+    }
+
+    public String getJsonPayload(){
+        UserDto dto = new UserDto();
+        dto.setNome(NOME);
+        dto.setEmail(EMAIL);
+        dto.setSenha(SENHA);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        return mapper.writeValueAsString(dto);
+    }
+
+}
