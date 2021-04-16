5360b0966bc6f329711be5ad769aeaf557349c58
Dherkyan Ribeiro da Silva <dherkyan@gmail.com>
2021-04-16 00:51:09 -0400

    14. Testes buscar por email

--- src/test/java/com/softwarefoundation/walletapi/repository/UserRepositoryTest.java
+++ src/test/java/com/softwarefoundation/walletapi/repository/UserRepositoryTest.java
@@ -1,27 +1,56 @@
 package com.softwarefoundation.walletapi.repository;
 
 import com.softwarefoundation.walletapi.entity.User;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 @SpringBootTest
 class UserRepositoryTest {
 
+    static final String EMAIL = "alice@gmail.com";
+
     @Autowired
     UserRepository userRepository;
+
+    @BeforeAll
+    public void setUp(){
+        User user = new User();
+        user.setNome("Alice");
+        user.setEmail(EMAIL);
+        user.setSenha("123");
+
+        userRepository.save(user);
+    }
+
+    @AfterAll
+    public void tearDown(){
+        userRepository.deleteAll();
+    }
 
     @Test
     void testSave() {
         User user = new User();
         user.setNome("Alice");
-        user.setEmail("alice@gmail.com");
+        user.setEmail(EMAIL);
         user.setSenha("123");
 
         User userRetorno = userRepository.save(user);
 
-        Assertions.assertNotNull(userRetorno);
+        assertNotNull(userRetorno);
+    }
+
+    public void testFindByEmail(){
+        Optional<User> userRetorno = userRepository.findByEmail(EMAIL);
+
+        assertTrue(userRetorno.isPresent());
+        assertEquals(userRetorno.get().getEmail(), EMAIL);
+
     }
 
 }
