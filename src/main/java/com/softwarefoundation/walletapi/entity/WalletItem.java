604f7e10fe406e45f22479e7f023acb4219c6d90
Dherkyan R. Silva <dherkyan@gmail.com>
2024-10-30 09:50:41 -0300

    Configuração gradle 7.2

--- src/main/java/com/softwarefoundation/walletapi/entity/WalletItem.java
+++ src/main/java/com/softwarefoundation/walletapi/entity/WalletItem.java
@@ -5,7 +5,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.io.Serializable;
